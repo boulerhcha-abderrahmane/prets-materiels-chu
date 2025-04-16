9f629bb34d8301bd6dd4da93da29c9cfecc753bd
Votre Nom <votre.email@example.com>
2025-04-16 20:13:50 +0100

    d
    d

---
+++ config/config.php
@@ -0,0 +1,18 @@
+    <?php
+
+// Paramètres de connexion à la base de données
+$host = 'localhost';
+$dbname = 'test6';
+$username = 'root';
+$password = '';
+
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch(PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+
+?>
